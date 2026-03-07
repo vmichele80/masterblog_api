681d331508472505f9a87d54f982c9fd84a1bc6b
Mike <vmichele80@hotmail.com>
2026-03-07 13:18:44 +0100

    implemented the POST call to api/posts endpoint to add a post

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
+# deaktiviert die sortierung se elementen in der Json
+app.config["JSON_SORT_KEYS"] = False
+app.json.sort_keys = False
+
 CORS(app)  # This will enable CORS for all routes
 
 POSTS = [
@@ -9,10 +13,42 @@ POSTS = [
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
 
+def validate_post_data(data):
+    if "title" not in data or "content" not in data:
+        return False
+    return True
 
-@app.route('/api/posts', methods=['GET'])
+
+@app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
-    return jsonify(POSTS)
+
+    if request.method == 'POST':
+        # Get the new post data from the client
+        new_post = request.get_json()
+        # this validates the data is in the correct format
+        if not validate_post_data(new_post):
+            return jsonify({"error": "Title and/or Content of the post are missing"}), 400
+
+        # Generate a new ID for the book
+        new_id = max(post['id'] for post in POSTS) + 1
+        new_post['id'] = new_id
+
+        saved_post = {
+            "id": new_post["id"],
+            "title": new_post["title"],
+            "content": new_post["content"]
+        }
+
+        # Add the new book to our list
+        POSTS.append(saved_post)
+
+        # Return the new book data to the client
+        return jsonify(saved_post), 201
+
+
+    else:
+        # it means it is a GET request
+        return jsonify(POSTS)
 
 
 if __name__ == '__main__':
