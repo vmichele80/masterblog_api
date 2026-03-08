3bbd9cc99e1b17b9b6eee5bac3aafc3ab51d4dec
Mike <vmichele80@hotmail.com>
2026-03-08 20:31:03 +0100

    Implemented the end point for deleting the post by a given id

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -22,6 +22,14 @@ def validate_post_data(data):
 
     return True
 
+def find_post_by_id(post_id):
+    """ Find the post with the id `post_id`.
+    If there is no post with this id, return None. """
+    for post in POSTS:
+        if post["id"] == post_id:
+            return post
+
+    return None
 
 @app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
@@ -57,7 +65,20 @@ def get_posts():
 # Need to implement the delete end point
 @app.route('/api/posts/<int:id>', methods=['DELETE'])
 def delete_post(id):
-    pass
+    # Find the post with the given ID
+    post = find_post_by_id(id)
+
+    # If the post wasn't found, return a 404 error
+    if post is None:
+        return 'The selected post was not found', 404
+
+    # Remove the book from the list
+    POSTS.remove(post)
+
+    delete_confirmation_message = {"message": f"Post with id {id} has been deleted successfully."}
+
+    return jsonify(delete_confirmation_message)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
